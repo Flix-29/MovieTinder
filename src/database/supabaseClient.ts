de693e22aac5ee470632d07c1bb7d059da01aea2
flix29 <flix.1895@web.de>
2025-03-10 22:24:46 +0100

    add public keys

--- src/database/supabaseClient.ts
+++ src/database/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY
+    "https://pszjkfcllfuiicnjqimk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzemprZmNsbGZ1aWljbmpxaW1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMDQ3NTcsImV4cCI6MjA1Njc4MDc1N30.7uziq2NeyrbqMFHb-cT17juNy8JT-8bnC09EFExxdP4"
 )
